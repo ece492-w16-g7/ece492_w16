cd2a54c57d4759e2fe84d36d48146ac50565ce49
Patrick <pkuczera@e3-11-10.ece.ualberta.ca>
2016-02-29 16:04:36 -0700

    Added Video-In to VGA Qsys && Centroids

--- quartus_project_main_files/niosII_gesture_detection.vhd
+++ quartus_project_main_files/niosII_gesture_detection.vhd
@@ -1,3 +1,12 @@
+ -- Nancy Minderman
+-- nancy.minderman@ualberta.ca
+-- This file makes extensive use of Altera template structures.
+-- This file is the top-level file for lab 1 winter 2014 for version 12.1sp1 on Windows 7
+
+
+-- A library clause declares a name as a library.  It 
+-- does not create the library; it simply forward declares 
+-- it. 
 library ieee;
 
 -- Commonly imported packages:
@@ -12,158 +21,123 @@ library ieee;
 	use ieee.VITAL_Primitives.all;
 	
 	use work.DE2_CONSTANTS.all;
-
-entity niosII_gesture_detection is
 	
+entity niosII_gesture_detection is
 	port
 	(
-	KEY		 : in  std_logic_vector (0 downto 0);
-	CLOCK_50 : in  std_logic;
-	CLOCK_27 : in std_logic;
+		-- Input ports and 50 MHz Clock
+		KEY		: in  std_logic_vector (0 downto 0);
+		SW			: in 	std_logic_vector (17 downto 0);
+		CLOCK_50	: in  std_logic;
 
-	I2C_SCLK : out std_logic;
-	I2C_SDAT : inout std_logic := 'X';
-
-
-    TD_CLK27 : in    std_logic                     := 'X';             -- TD_CLK27
-	TD_DATA  : in std_logic_vector(7 downto 0) := (others => 'X');
-	TD_HS    : in std_logic := 'X';
-	TD_VS    : in std_logic := 'X';
-	TD_RESET : out std_logic;
-
-	SRAM_CONT_ADDR : out std_logic_vector(17 downto 0);
-	SRAM_CONT_DQ : inout std_logic_vector(15 downto 0);
-	SRAM_CONT_WE_N : out std_logic;
-	SRAM_CONT_OE_N : out std_logic;
-	SRAM_CONT_UB_N : out std_logic;
-	SRAM_CONT_LB_N : out std_logic;
-	SRAM_CONT_CE_N : out std_logic;
-
-	VGA_R : out std_logic_vector(9 downto 0);
-	VGA_G : out std_logic_vector(9 downto 0);
-	VGA_B : out std_logic_vector(9 downto 0);
-	VGA_HS		: out std_logic;
-	VGA_VS		: out std_logic;
-	VGA_SYNC		: out std_logic;
-	VGA_BLANK	: out std_logic;
-	VGA_CLK		: out std_logic;
-	
-	SRAM_ADDR : out std_logic_vector(17 downto 0);
-	SRAM_DQ : inout std_logic_vector(15 downto 0);
-	SRAM_WE_N : out std_logic;
-	SRAM_OE_N : out std_logic;
-	SRAM_UB_N : out std_logic;
-	SRAM_LB_N : out std_logic;
-	SRAM_CE_N : out std_logic;
-	
-	-- LCD on board
-	LCD_BLON	: out std_logic;
-	LCD_ON	: out std_logic;
-	LCD_DATA	: inout DE2_LCD_DATA_BUS;
-	LCD_RS	: out std_logic;
-	LCD_EN	: out std_logic;
-	LCD_RW	: out std_logic
+		-- SRAM on board
+		
+		SRAM_ADDR	:	out	DE2_SRAM_ADDR_BUS;
+		SRAM_DQ		:	inout DE2_SRAM_DATA_BUS;
+		SRAM_WE_N	:	out	std_logic;
+		SRAM_OE_N	:	out	std_logic;
+		SRAM_UB_N	:	out 	std_logic;
+		SRAM_LB_N	:	out	std_logic;
+		SRAM_CE_N	:	out	std_logic;
+		
+		-- VGA Controller
+		VGA_R       : OUT STD_LOGIC_VECTOR (9 downto 0);
+		VGA_B       : OUT STD_LOGIC_VECTOR (9 downto 0);
+		VGA_G       : OUT STD_LOGIC_VECTOR (9 downto 0);
+		VGA_CLK     : OUT STD_LOGIC;
+		VGA_BLANK   : OUT STD_LOGIC;
+		VGA_HS      : OUT STD_LOGIC;
+		VGA_VS      : OUT STD_LOGIC;
+		VGA_SYNC    : OUT STD_LOGIC;
+		
+		I2C_SDAT    : inout std_logic                     := 'X';            
+      I2C_SCLK    : out   std_logic;
+		
+		TD_CLK27    : in    std_logic                     := 'X';
+      TD_DATA     : in    std_logic_vector(7 downto 0)  := (others => 'X');
+      TD_HS       : in    std_logic                     := 'X';
+      TD_VS       : in    std_logic                     := 'X';
+      TD_RESET    : out   std_logic	
 	);
 end niosII_gesture_detection;
 
 architecture structure of niosII_gesture_detection is
-    component niosII_system is
-        port (
-            clk_clk                                         : in    std_logic                     := 'X';             -- clk
-            reset_reset_n                                   : in    std_logic                     := 'X';             -- reset_n
+	component niosII_system is
+	  port (
+			clk_clk                                  : in    std_logic                     := 'X';             -- clk
+			reset_reset_n                            : in    std_logic                     := 'X';             -- reset_n
+			pixel_buffer_external_interface_DQ       : inout DE2_SRAM_DATA_BUS := (others => 'X');             -- DQ
+			pixel_buffer_external_interface_ADDR     : out   DE2_SRAM_ADDR_BUS;                                -- ADDR
+			pixel_buffer_external_interface_LB_N     : out   std_logic;                                        -- LB_N
+			pixel_buffer_external_interface_UB_N     : out   std_logic;                                        -- UB_N
+			pixel_buffer_external_interface_CE_N     : out   std_logic;                                        -- CE_N
+			pixel_buffer_external_interface_OE_N     : out   std_logic;                                        -- OE_N
+			pixel_buffer_external_interface_WE_N     : out   std_logic;                                        -- WE_N
+			av_config_external_interface_SDAT        : inout std_logic                     := 'X';             -- SDAT
+			av_config_external_interface_SCLK        : out   std_logic;                                        -- SCLK
+			decoder_external_interface_TD_CLK27      : in    std_logic                     := 'X';             -- TD_CLK27
+			decoder_external_interface_TD_DATA       : in    std_logic_vector(7 downto 0)  := (others => 'X'); -- TD_DATA
+			decoder_external_interface_TD_HS         : in    std_logic                     := 'X';             -- TD_HS
+			decoder_external_interface_TD_VS         : in    std_logic                     := 'X';             -- TD_VS
+			decoder_external_interface_TD_RESET      : out   std_logic;                                        -- TD_RESET
+			decoder_external_interface_overflow_flag : out   std_logic;                                        -- overflow_flag
+			vga_controller_external_interface_CLK    : out   std_logic;                                        -- CLK
+			vga_controller_external_interface_HS     : out   std_logic;                                        -- HS
+			vga_controller_external_interface_VS     : out   std_logic;                                        -- VS
+			vga_controller_external_interface_BLANK  : out   std_logic;                                        -- BLANK
+			vga_controller_external_interface_SYNC   : out   std_logic;                                        -- SYNC
+			vga_controller_external_interface_R      : out   std_logic_vector(9 downto 0);                     -- R
+			vga_controller_external_interface_G      : out   std_logic_vector(9 downto 0);                     -- G
+			vga_controller_external_interface_B      : out   std_logic_vector(9 downto 0)                      -- B
+	  );
+	end component niosII_system;
 
-            video_decoder_external_interface_TD_CLK27       : in    std_logic                     := 'X';             -- TD_CLK27
-            video_decoder_external_interface_TD_DATA        : in    std_logic_vector(7 downto 0)  := (others => 'X'); -- TD_DATA
-            video_decoder_external_interface_TD_HS          : in    std_logic                     := 'X';             -- TD_HS
-            video_decoder_external_interface_TD_VS          : in    std_logic                     := 'X';             -- TD_VS
-            video_decoder_external_interface_TD_RESET       : out   std_logic;                                        -- TD_RESET
-            --video_decoder_external_interface_overflow_flag  : out   std_logic;                                        -- overflow_flag
-
-            sram_controller_external_interface_DQ           : inout std_logic_vector(15 downto 0) := (others => 'X'); -- DQ
-            sram_controller_external_interface_ADDR         : out   std_logic_vector(17 downto 0);                    -- ADDR
-            sram_controller_external_interface_LB_N         : out   std_logic;                                        -- LB_N
-            sram_controller_external_interface_UB_N         : out   std_logic;                                        -- UB_N
-            sram_controller_external_interface_CE_N         : out   std_logic;                                        -- CE_N
-            sram_controller_external_interface_OE_N         : out   std_logic;                                        -- OE_N
-            sram_controller_external_interface_WE_N         : out   std_logic;                                        -- WE_N
-
-            video_vga_controller_0_external_interface_CLK   : out   std_logic;                                        -- CLK
-            video_vga_controller_0_external_interface_HS    : out   std_logic;                                        -- HS
-            video_vga_controller_0_external_interface_VS    : out   std_logic;                                        -- VS
-            video_vga_controller_0_external_interface_BLANK : out   std_logic;                                        -- BLANK
-            video_vga_controller_0_external_interface_SYNC  : out   std_logic;                                        -- SYNC
-            video_vga_controller_0_external_interface_R     : out   std_logic_vector(9 downto 0);                     -- R
-            video_vga_controller_0_external_interface_G     : out   std_logic_vector(9 downto 0);                     -- G
-            video_vga_controller_0_external_interface_B     : out   std_logic_vector(9 downto 0);                     -- B
-				
-			sram_0_external_interface_DQ                    : inout std_logic_vector(15 downto 0) := (others => 'X'); -- DQ
-            sram_0_external_interface_ADDR                  : out   std_logic_vector(17 downto 0);                    -- ADDR
-            sram_0_external_interface_LB_N                  : out   std_logic;                                        -- LB_N
-            sram_0_external_interface_UB_N                  : out   std_logic;                                        -- UB_N
-            sram_0_external_interface_CE_N                  : out   std_logic;                                        -- CE_N
-            sram_0_external_interface_OE_N                  : out   std_logic;                                        -- OE_N
-            sram_0_external_interface_WE_N                  : out   std_logic;          
-				
-			character_lcd_0_external_interface_DATA : inout DE2_LCD_DATA_BUS  := (others => 'X'); -- DATA
-            character_lcd_0_external_interface_ON   : out   std_logic;                                        -- ON
-            character_lcd_0_external_interface_BLON : out   std_logic;                                        -- BLON
-            character_lcd_0_external_interface_EN   : out   std_logic;                                        -- EN
-            character_lcd_0_external_interface_RS   : out   std_logic;                                        -- RS
-            character_lcd_0_external_interface_RW   : out   std_logic;   
-
-            audio_video_config_external_interface_SDAT      : inout std_logic                     := 'X';             -- SDAT
-            audio_video_config_external_interface_SCLK      : out   std_logic                                         -- SCLK
-        );
-    end component niosII_system;
-
+	--	These signals are for matching the provided IP core to
+	-- The specific SDRAM chip in our system	 
+	--signal BA	: std_logic_vector (1 downto 0);
+	--signal DQM	:	std_logic_vector (1 downto 0);
+	 
 begin
 
+	--DRAM_BA_1 <= BA(1);
+	--DRAM_BA_0 <= BA(0);
+	
+	--DRAM_UDQM <= DQM(1);
+	--DRAM_LDQM <= DQM(0);
+	
+	-- Component Instantiation Statement (optional)
+	 
     u0 : component niosII_system
         port map (
-            clk_clk                                         => CLOCK_50,       -- clk.clk
-            reset_reset_n                                   => KEY(0),            -- reset.reset_n
-
-            video_decoder_external_interface_TD_CLK27       => CLOCK_27,       -- video_decoder_external_interface.TD_CLK27
-            video_decoder_external_interface_TD_DATA        => TD_DATA,        -- .TD_DATA
-            video_decoder_external_interface_TD_HS          => TD_HS,          -- .TD_HS
-            video_decoder_external_interface_TD_VS          => TD_VS,          -- .TD_VS
-            video_decoder_external_interface_TD_RESET       => TD_RESET,       -- .TD_RESET
-            --video_decoder_external_interface_overflow_flag  => CONNECTED_TO_video_decoder_external_interface_overflow_flag,  -- .overflow_flag
-
-            sram_controller_external_interface_DQ           => SRAM_CONT_DQ,           -- sram_controller_external_interface.DQ
-            sram_controller_external_interface_ADDR         => SRAM_CONT_ADDR,         -- .ADDR
-            sram_controller_external_interface_LB_N         => SRAM_CONT_LB_N,         -- .LB_N
-            sram_controller_external_interface_UB_N         => SRAM_CONT_UB_N,         -- .UB_N
-            sram_controller_external_interface_CE_N         => SRAM_CONT_CE_N,         -- .CE_N
-            sram_controller_external_interface_OE_N         => SRAM_CONT_OE_N,         -- .OE_N
-            sram_controller_external_interface_WE_N         => SRAM_CONT_WE_N,         -- .WE_N
-
-            video_vga_controller_0_external_interface_CLK   => VGA_CLK,   --video_vga_controller_0_external_interface.CLK
-            video_vga_controller_0_external_interface_HS    => VGA_HS,    -- .HS
-            video_vga_controller_0_external_interface_VS    => VGA_VS,    -- .VS
-            video_vga_controller_0_external_interface_BLANK => VGA_BLANK, -- .BLANK
-            video_vga_controller_0_external_interface_SYNC  => VGA_SYNC,  -- .SYNC
-            video_vga_controller_0_external_interface_R     => VGA_R,     -- .R
-            video_vga_controller_0_external_interface_G     => VGA_G,     -- .G
-            video_vga_controller_0_external_interface_B     => VGA_B,     -- .B
+            clk_clk                                  => CLOCK_50,   --   clk.clk
+            reset_reset_n                            => KEY(0),     --   reset.reset_n
 				
-			sram_0_external_interface_DQ                    => SRAM_DQ,                    --                 sram_0_external_interface.DQ
-            sram_0_external_interface_ADDR                  => SRAM_ADDR,                  --                                          .ADDR
-            sram_0_external_interface_LB_N                  => SRAM_LB_N,                  --                                          .LB_N
-            sram_0_external_interface_UB_N                  => SRAM_UB_N,                  --                                          .UB_N
-            sram_0_external_interface_CE_N                  => SRAM_CE_N,                  --                                          .CE_N
-            sram_0_external_interface_OE_N                  => SRAM_OE_N,                  --                                          .OE_N
-            sram_0_external_interface_WE_N                  => SRAM_WE_N,      
+            pixel_buffer_external_interface_DQ       => SRAM_DQ,    --  .DQ
+            pixel_buffer_external_interface_ADDR     => SRAM_ADDR,  --  .ADDR
+            pixel_buffer_external_interface_LB_N     => SRAM_LB_N,  --  .LB_N
+            pixel_buffer_external_interface_UB_N     => SRAM_UB_N,  --  .UB_N
+            pixel_buffer_external_interface_CE_N     => SRAM_CE_N,  --  .CE_N
+            pixel_buffer_external_interface_OE_N     => SRAM_OE_N,  --  .OE_N
+            pixel_buffer_external_interface_WE_N     => SRAM_WE_N,  --  .WE_N
 				
-			character_lcd_0_external_interface_DATA => LCD_DATA, 
-            character_lcd_0_external_interface_ON   => LCD_ON,   
-            character_lcd_0_external_interface_BLON => LCD_BLON, 
-            character_lcd_0_external_interface_EN   => LCD_EN,   
-            character_lcd_0_external_interface_RS   => LCD_RS,   
-            character_lcd_0_external_interface_RW   => LCD_RW,
-
-            audio_video_config_external_interface_SDAT      => I2C_SDAT,      -- audio_video_config_external_interface.SDAT
-            audio_video_config_external_interface_SCLK      => I2C_SCLK       -- .SCLK
+            av_config_external_interface_SDAT        => I2C_SDAT,   --  .SDAT
+            av_config_external_interface_SCLK        => I2C_SCLK,   --  .SCLK
+				
+            decoder_external_interface_TD_CLK27      => TD_CLK27,   --  .TD_CLK27
+            decoder_external_interface_TD_DATA       => TD_DATA,    --  .TD_DATA
+            decoder_external_interface_TD_HS         => TD_HS,      --  .TD_HS
+            decoder_external_interface_TD_VS         => TD_VS,      --  .TD_VS
+            decoder_external_interface_TD_RESET      => TD_RESET,   --  .TD_RESET
+            --decoder_external_interface_overflow_flag => <>, -- .overflow_flag
+           
+			   vga_controller_external_interface_CLK    => VGA_CLK,    --  .CLK
+            vga_controller_external_interface_HS     => VGA_HS,     --  .HS
+            vga_controller_external_interface_VS     => VGA_VS,     --  .VS
+            vga_controller_external_interface_BLANK  => VGA_BLANK,  --  .BLANK
+            vga_controller_external_interface_SYNC   => VGA_SYNC,   --  .SYNC
+            vga_controller_external_interface_R      => VGA_R,      --  .R
+            vga_controller_external_interface_G      => VGA_G,      --  .G
+            vga_controller_external_interface_B      => VGA_B       --  .B
         );
 
 end structure;
@@ -189,3 +163,5 @@ package DE2_CONSTANTS is
 	type DE2_SRAM_DATA_BUS  is array(15 downto 0) of std_logic;
 	
 end DE2_CONSTANTS;
+
+
---
+++ quartus_project_main_files/vhdl/LED_detector.vhd
@@ -0,0 +1,144 @@
+LIBRARY ieee;
+USE ieee.std_logic_1164.all;
+USE ieee.std_logic_unsigned.all;
+USE ieee.std_logic_misc.all;
+use IEEE.NUMERIC_STD.all;
+
+
+ENTITY LED_detector IS 
+
+-- *****************************************************************************
+-- *                             Generic Declarations                          *
+-- *****************************************************************************
+	
+GENERIC (
+	
+	IW		:INTEGER									:= 23;
+	OW		:INTEGER									:= 23;
+	
+	EIW	:INTEGER									:= 1;
+	EOW	:INTEGER									:= 1
+	
+);
+-- *****************************************************************************
+-- *                             Port Declarations                             *
+-- *****************************************************************************
+PORT (
+
+	-- Inputs
+	clk								:IN		STD_LOGIC;
+	reset								:IN		STD_LOGIC;
+
+	stream_in_data					:IN		STD_LOGIC_VECTOR(IW DOWNTO  0);	
+	stream_in_startofpacket		:IN		STD_LOGIC;
+	stream_in_endofpacket		:IN		STD_LOGIC;
+	stream_in_empty				:IN		STD_LOGIC_VECTOR(EIW DOWNTO  0);	
+	stream_in_valid				:IN		STD_LOGIC;
+
+	stream_out_ready				:IN		STD_LOGIC;
+	
+	--threshold_sw               :IN      STD_LOGIC_VECTOR(7 DOWNTO 0);
+
+	-- Bidirectional
+
+	-- Outputs
+	stream_in_ready				:BUFFER	STD_LOGIC;
+
+	stream_out_data				:BUFFER	STD_LOGIC_VECTOR(OW DOWNTO  0);	
+	stream_out_startofpacket	:BUFFER	STD_LOGIC;
+	stream_out_endofpacket		:BUFFER	STD_LOGIC;
+	stream_out_empty				:BUFFER	STD_LOGIC_VECTOR(EOW DOWNTO  0);	
+	stream_out_valid				:BUFFER	STD_LOGIC;
+	
+	position_irq               :OUT     STD_LOGIC;
+	m_position                 :OUT     STD_LOGIC_VECTOR(9 DOWNTO 0);
+	n_position                 :OUT     STD_LOGIC_VECTOR(9 DOWNTO 0)
+);
+
+END LED_detector;
+
+ARCHITECTURE detector OF LED_detector IS
+
+	TYPE FRAME_STATE IS (PROCESSING, IDLE);
+
+	SIGNAL current_state : FRAME_STATE := IDLE;
+	SIGNAL y_component   : STD_LOGIC_VECTOR(7 DOWNTO 0);
+	
+	VARIABLE m_counter          : INTEGER := 0;
+	VARIABLE n_counter          : INTEGER := 0;
+	VARIABLE consecutive_pixels : INTEGER := 0;
+	VARIABLE max_consecutive    : INTEGER := 0;
+	VARIABLE led_detected       : STD_LOGIC := '0';
+	
+	SIGNAL m_stored : STD_LOGIC_VECTOR(9 DOWNTO 0);
+	SIGNAL n_stored : STD_LOGIC_VECTOR(9 DOWNTO 0);
+	
+	CONSTANT frame_size_row     : INTEGER := 640;
+	--CONSTANT frame_size_column  : INTEGER := 240;
+	CONSTANT threshold_value    : INTEGER := 250;
+	CONSTANT consecutive_thresh : INTEGER := 5;
+
+BEGIN
+	
+	y_component <= stream_in_data(7 DOWNTO 0);
+	
+	stream_out_data <= stream_in_data;
+	stream_out_startofpacket <= stream_in_startofpacket;
+	stream_out_endofpacket <= stream_in_endofpacket;
+	stream_out_empty <= stream_in_empty;
+	stream_out_valid <= stream_in_valid;
+	
+	PROCESS (clk)
+	BEGIN
+		IF clk'EVENT AND clk = '1' THEN
+			CASE current_state IS	
+				WHEN PROCESSING =>
+					IF (UNSIGNED(y_component) > threshold_value) THEN
+						consecutive_pixels := consecutive_pixels + 1;
+					ELSIF (consecutive_pixels > consecutive_thresh) THEN
+						led_detected := '1';
+						IF (consecutive_pixels > max_consecutive) THEN
+							max_consecutive := consecutive_pixels;
+							m_stored <= m_counter;
+							n_stored <= n_counter - STD_LOGIC_VECTOR(consecutive_pixels/ 2);
+						END IF;
+					ELSE
+						consecutive_pixels := 0;
+					END IF;
+					
+					n_counter := n_counter +1;
+					IF (n_counter = frame_size_row) THEN
+						n_counter := 0;
+						m_counter := m_counter +1;
+					END IF;
+					
+				WHEN IDLE =>
+					NULL;
+			END CASE;
+		END IF;
+	END PROCESS;
+	
+	PROCESS (clk)
+	BEGIN
+		IF clk'EVENT AND clk = '1' THEN
+			IF (stream_in_startofpacket = '1') THEN
+				position_irq <= '0';
+				m_counter := 0;
+				n_counter := 0;
+				consecutive_pixels := 0;
+				max_consecutive := 0;
+				led_detected <= '0';
+				current_state <= PROCESSING;
+			ELSIF (stream_in_endofpacket = '1') AND (led_detected = '1') THEN
+				position_irq <= '1';
+				m_position <= m_stored;
+				n_position <= n_stored;
+				current_state <= IDLE;
+			ELSIF (stream_in_valid = '1') THEN 
+				current_state <= PROCESSING;
+			ELSE
+				current_state <= IDLE;
+			END IF;
+		END IF;
+	END PROCESS;
+END detector;
